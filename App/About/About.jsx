c7fbe978df6b45b2bc08148fed870c7cbcdfd2e2
Katya Weicht <katya.weicht@gmail.com>
2025-06-03 14:13:00 -0600

    Change files to jsx

---
+++ App/About/About.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Links from "./Links/Links"
+
+function About() {
+  return (
+    <div id="about">
+      About!
+      <Links/>
+    </div>
+  )
+}
+
+export default About;
